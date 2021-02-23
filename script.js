83fc8517995e28d218e8dab0bc4f0b076c574911
jaimerobbins <robbins_jaime@hotmail.com>
2021-02-23 12:22:11 -0500

    movie results section formatting

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
 
 const movieApp = {};
 
+//get movie options from TMDB API
 movieApp.getData = (genre) => {
     const url = new URL('https://api.themoviedb.org/3/discover/movie')
     url.search = new URLSearchParams({
@@ -24,85 +25,147 @@ movieApp.getData = (genre) => {
             //parse the response into JSON
             return response.json()
         }).then(function (jsonResponse) {
-            // work with the data from the API
 
-            //check if results have poster path, re-assign if not
-            let movie = movieApp.randomItem(jsonResponse.results);
-            while (!movie.poster_path) {
-                movie = movieApp.randomItem(jsonResponse.results);
-            }
+            //filter returned array to exclude those without a poster path
+            const newArr = jsonResponse.results.filter((el) => {
+                if (el.poster_path) {
+                    return el;
+                };
+            });
 
+            //select random movie
+            const movie = movieApp.randomItem(newArr);
+
+            //call function to display movie
             movieApp.displayMovie(movie);
-            
         })
     }
 
-// movieApp.getTodaysDate = () => {
-//     let today = new Date();
-//     const dd = String(today.getDate()).padStart(2, '0');
-//     const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-//     const yyyy = today.getFullYear();
-
-//     today = mm + '-' + dd + '-' + yyyy;
-//     return(today);
-// }
-    
+//display random movie
 movieApp.displayMovie = (movie) => {
-    console.log(movie);
 
-    const movieTitle = document.querySelector('.movie-results h2');
-    const movieImg = document.querySelector('.movie-results img');
-    const movieOverview = document.querySelector('.movie-results .overview');
-    const movieVoteAvg = document.querySelector('.movie-results .vote-avg');
-    const movieReleaseDt = document.querySelector('.movie-results .release-dt');
+    //construct elements
+        //create poster img container
+        const posterImgContainer = document.createElement('div');
+        posterImgContainer.classList.add('movie-img');
 
-    movieTitle.textContent = movie.title;
-    movieOverview.textContent = movie.overview;
-    movieImg.src = "https://image.tmdb.org/t/p/w500/" + movie.poster_path;
-    movieImg.alt = movie.title + " movie poster"
-    // movieVoteAvg.textContent = "Vote Average: " + movie.vote_average;
-    movieReleaseDt.textContent = "Release Date: " + movie.release_date;
+        //append poster image to container
+        const posterImg = document.createElement('img');
+        posterImg.src = "https://image.tmdb.org/t/p/w500/" + movie.poster_path;
+        posterImg.alt = movie.title + " movie poster";
+        posterImgContainer.appendChild(posterImg);
 
-    //** stars!!! would need to remove p in html...might be easier to set all up in JS? **
-    console.log(movie.vote_average)
-    movieVoteAvg.innerHTML = `
-        <div class="vote-average">
-            <p class="sr-only">Vote Average: ${movie.vote_average}</p>
-            <div class="star-ratings">
-                <div class="empty-ratings">
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                    <i class="far fa-star"></i>
-                </div> 
-                <div class="fill-ratings" style="width: ${movie.vote_average * 10}%;">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-            </div>
-            <p>(${movie.vote_count} votes)</p>
-        </div>`
+        //create overview container
+        const overviewContainer = document.createElement('div');
+        overviewContainer.classList.add('movie-txt');
+
+        //create overview title
+        const overviewTitle = document.createElement('h3');
+        overviewTitle.textContent = 'Overview:';
+
+        //create star rating containers
+        const ratingsContainer = document.createElement('div');
+        ratingsContainer.classList.add('vote-average')
+        const starContainer = document.createElement('div');
+        starContainer.classList.add('star-ratings')
+        const starFilledContainer = document.createElement('div');
+        starFilledContainer.classList.add('fill-ratings')
+        const starEmptyContainer = document.createElement('div');
+        starEmptyContainer.classList.add('empty-ratings')
+
+        ratingsContainer.appendChild(starContainer);
+        starContainer.appendChild(starEmptyContainer);
+        starContainer.appendChild(starFilledContainer);
+
+        //add sr-only rating text
+        const srText = document.createElement('p');
+        srText.classList.add('sr-only');
+        srText.textContent = `Vote Average: ${movie.vote_average} / 10`
+        ratingsContainer.appendChild(srText);
+
+        //add stars
+        movieApp.addStars('fa fa-star', starFilledContainer);
+        movieApp.addStars('far fa-star', starEmptyContainer);
+
+        //update width of filled stars to rating value
+        starFilledContainer.setAttribute('style', `width: ${movie.vote_average * 10}%;`);
+
+    //add vote count
+    const voteCnt = document.createElement('p');
+    voteCnt.textContent = `(${movie.vote_count} votes)`;
+    ratingsContainer.appendChild(voteCnt);
+
+    //append items to container
+        //create + append overview text
+        const overview = document.createElement('p');
+        overview.textContent = movie.overview;
+        overview.classList.add('overview');
+
+        //create release date container
+        const releaseDtContainer = document.createElement('div');
+        releaseDtContainer.classList.add('sub-heading');
+
+        //append release date header and value
+        const releaseDtHeader = document.createElement('h3');
+        releaseDtHeader.textContent = 'Release Date: ';
+        const releaseDtValue = document.createElement('p');
+        releaseDtValue.textContent = movie.release_date;
+        
+        releaseDtContainer.appendChild(releaseDtHeader)
+        releaseDtContainer.appendChild(releaseDtValue);
+
+    //add to page
+        //locate + clear results container
+        const resultsContainer = document.querySelector('.movie-results-section div');
+        resultsContainer.innerHTML = '';
+        resultsContainer.classList.add('movie-results-container');
+
+        //add movie title
+        resultsContainer.appendChild(document.createElement('h2')).textContent = movie.title;
+
+        //create details container + append to results container
+        const movieDetailsContainer = document.createElement('div');
+        movieDetailsContainer.classList.add('movie-details')
+        resultsContainer.appendChild(movieDetailsContainer);
+        
+        //append all overview items to overview container
+        overviewContainer.appendChild(overviewTitle);
+        overviewContainer.appendChild(ratingsContainer);
+        overviewContainer.appendChild(overview);
+        overviewContainer.appendChild(releaseDtContainer);
+
+        //append image and all overview items to details container
+        movieDetailsContainer.appendChild(posterImgContainer);
+        movieDetailsContainer.appendChild(overviewContainer);
 }
 
+
+//||||| helper functions |||||
+//create star icons
+movieApp.addStars = (iconClass, iconContainer, num = 10) => {
+    for (let j = 0; j < num; j++) {
+        const starIcon = document.createElement('i')
+        starIcon.setAttribute("class", iconClass);
+        iconContainer.appendChild(starIcon);
+    }
+}
+
+//reformat date
+movieApp.reformatDate = (dt) => {
+    const date = new Date(dt);
+    const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+    const month = new Intl.DateTimeFormat('en', { month: 'short' }).format(date);
+    const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
+    return `${month} ${day}, ${year}`;
+}
+
+//select random item
 movieApp.randomItem = (arr) => {
     const randomEl = Math.floor(Math.random() * arr.length);
     return arr[randomEl];
 }
 
+//||||| initialize |||||
 movieApp.init = () => {
 
     const formEl = document.querySelector('form');
